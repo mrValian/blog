e6ed29b5f07438be817f0a708f4b9e13ed8b3275
Aleksandr Fartushniy <valian55029@gmail.com>
2025-09-25 17:18:41 +0300

    Реализовать  основной компонент блога с роутингом

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
+import { Routes, Route } from 'react-router-dom';
+
 import styled from 'styled-components';
 
-const Div = styled.div`
-	text-align: center;
-	color: red;
-	font-size: 50px;
+const Content = styled.div`
+	padding: 120px 0;
 `;
 
-export const App = () => {
+const H2 = styled.h2`
+	text-align: center;
+`;
 
+const Header = () => {
 	return (
-		<div>
-			<div><i className="fa fa-camera-retro fa-lg"></i> fa-lg</div>
-			<Div>456</Div>
-		</div>
+		<>
+			<h1>Шапка сайта</h1>
+		</>
+	);
+}
+
+const Footer = () => {
+	return (
+		<>
+			<h1>Подвал сайта</h1>
+		</>
+	);
+}
+
+export const App = () => {
+	return (
+		<>
+			<Header />
+			<Content>
+				<div>
+					<H2>Контент страницы</H2>
+					<p><i className="fa fa-camera-retro fa-lg"></i> fa-lg</p>
+					<Routes>
+						<Route path='/' element={<div>Main</div>} />
+						<Route path='/login' element={<div>Login</div>} />
+						<Route path='/register' element={<div>Registration</div>} />
+						<Route path='/users' element={<div>Users</div>} />
+						<Route path='/post' element={<div>New Post</div>} />
+						<Route path='/post/:postId' element={<div>Post</div>} />
+						<Route path='*' element={<div>Error</div>} />
+					</Routes>
+				</div>
+			</Content>
+			<Footer />
+		</>
 	);
 };
-
-
